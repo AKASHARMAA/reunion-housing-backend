e107edae41e572ec3fbb4be474b2f1af4ddd1fbd
Akash <akash.76648@gmail.com>
2023-11-24 17:38:44 +0530

    deleted unnecessary files

--- another.js
+++
@@ -1,7 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/reunion-housing';
-
-MongoClient.connect(url, function (err, client) {
-  console.log('Connected successfully to server');
-  client.close();
-});
--- another.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient(
-    'mongodb+srv://akash76648:QXGqheFCO1yq6bu6@akash.t5noifa.mongodb.net/?retryWrites=true&w=majority')
-print(client.list_database_names())
